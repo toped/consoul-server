5124de2c36e59695a13fddff26295b822e9178fd
Tope Daramola <tope.daram@gmail.com>
2021-10-28 12:09:50 -0500

    Initial Graphql server with Apollo subscriptions implemented. Note: The usage of Apollo's PubSub class is not recommended for production environments

---
+++ index.js
@@ -0,0 +1,73 @@
+require('dotenv').config()
+const admin = require('firebase-admin')
+const functions = require('firebase-functions')
+const { ApolloServer, PubSub, gql } = require('apollo-server')
+const port = process.env.PORT
+
+var Twit = require('twit')
+
+const typeDefs = require('./types')
+const resolvers = require('./resolvers')
+var serviceAccount = require('./service_key.json')
+
+var twitterClient = new Twit({
+	consumer_key: functions.config().twitterapi.consumer_key,
+	consumer_secret: functions.config().twitterapi.consumer_secret,
+	access_token: functions.config().twitterapi.access_token,
+	access_token_secret: functions.config().twitterapi.access_token_secret,
+	timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+	strictSSL: true // optional - requires SSL certificates to be valid.
+})
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: 'https://memez-f18eb.firebaseio.com/',
+	storageBucket: 'memez-f18eb.appspot.com'
+})
+
+const TimerModule = () => {
+	let timers = {}
+	const addTimer = (id, timer) => {
+		timers[id] = timer
+	}
+	const getTimers = () => timers
+	return {
+		addTimer,
+		getTimers
+	}
+}
+
+const timerModule = TimerModule()
+
+const context = async ({ req, res }) => {
+	return {
+		req,
+		res,
+		admin,
+		twitterClient,
+		timerModule,
+	}
+}
+
+const apolloServer = new ApolloServer({
+	typeDefs,
+	resolvers,
+	context,
+	cors: true,
+	subscriptions: {
+		path: '/subscriptions',
+		onConnect: (connectionParams, webSocket, context) => {
+			console.log('Client connected')
+			return context
+		},
+		onDisconnect: (webSocket, context) => {
+			console.log('Client disconnected')
+		},
+	},
+})
+
+apolloServer.listen().then(({ url }) => {
+	console.warn(`> 🚀  Apollo GraphQL Server ready on ${process.env.URL}${apolloServer.graphqlPath}`)
+	console.warn(`> 🚀 Subscription endpoint ready at ws://localhost:${port}${apolloServer.subscriptionsPath}`)
+	console.warn('Query at studio.apollographql.com/dev')
+}).catch(err => {throw err})
---
+++ types/Room.js
@@ -0,0 +1,55 @@
+const { gql } = require('apollo-server')
+
+const Room = gql`
+
+  # TYPES 
+  type GameRoom {
+    id: ID
+    host: String
+    slug: String
+    settings: GameSettings
+    #readonly
+  }
+
+  type GameUser {
+    displayName: String
+    status: String
+    uid: String
+  }
+
+  type GameSettings {
+    rounds: Int
+    maxPlayers: Int
+    timeLimit: Int
+  }
+
+  # INPUTS
+  input GameRoomInput {
+    id: ID
+    host: String
+    settings: GameSettingsInput
+  }
+
+  input GameSettingsInput {
+    rounds: Int
+    maxPlayers: Int
+    timeLimit: Int
+  }
+
+  extend type Query {
+    rooms(id: ID, host: String, slug: String): [GameRoom]
+  }
+
+  extend type Mutation {
+    createRoom(room: GameRoomInput): GameRoom
+    updateRoom(room: GameRoomInput): GameRoom
+    deleteRoom(host: String): String
+  }
+
+  type Subscription {
+    playerJoined: GameRoom
+    roomUpdated: GameRoom
+  }
+`
+
+module.exports = Room
