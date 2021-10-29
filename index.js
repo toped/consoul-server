d65187611a9d6e1b975a8c0722dd7390eee23cc9
Tope Daramola <tope.daram@gmail.com>
2021-10-28 21:18:05 -0500

    logging test message

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const typeDefs = require('./types')
 const resolvers = require('./resolvers')
 var serviceAccount = require('./service_key.json')
 
-console.log('consumer key should be', process.env.CONSUMER_KEY)
+console.log('consumer key should be', process.env)
 var twitterClient = new Twit({
 	consumer_key: process.env.CONSUMER_KEY,
 	consumer_secret: process.env.CONSUMER_SECRET,
